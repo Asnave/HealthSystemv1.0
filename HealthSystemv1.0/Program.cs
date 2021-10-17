a902d269e2cc0dbd58d952d9cf123d91af4a29b0
Abigail Evans <cdn_abigail@outlook.com>
2021-10-17 19:34:08 -0300

    Start Screen

--- HealthSystemv1.0/Program.cs
+++ HealthSystemv1.0/Program.cs
@@ -8,8 +8,43 @@ namespace HealthSystemv1._0
 {
     class Program
     {
+        static int health = 100;
+        static int shield = 100;
+        static int lives = 3;
         static void Main(string[] args)
         {
+
+            StartScreen();
+            Console.ReadKey(true);
         }
+
+        static void ShowHUD()
+        {
+
+        }
+
+        static void StartScreen()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("        |||||||||||     ||        ||||    ||||||          ||      ||||||||    ||||||||||    ||||||||||      ||||||          ||  ");
+            Console.WriteLine("        |||||||||||||   ||        ||||||  ||||||||        ||    ||||||||||    ||||      ||  |||||||||||     ||||||||        ||  ");
+            Console.WriteLine("          ||||||  |||||   ||||      ||||    ||||  ||||      ||  ||||    ||||    ||          ||||      ||||    ||||  ||||      ||");
+            Console.WriteLine("          ||||    |||||   ||||      ||||    ||||    ||      ||  ||||      ||    ||          ||||      ||||    ||||    ||      ||");
+            Console.WriteLine("          ||||      |||   ||||      ||||    ||||    ||||  ||||  ||||            ||||||      ||||      ||||    ||||    ||||  ||||");
+            Console.WriteLine("          ||||      |||   ||||      ||||    ||||      ||  ||||  ||||  ||||||    ||||||      ||||      ||||    ||||      ||  ||||");
+            Console.WriteLine("          ||||       ||     ||      ||||    ||||       || ||||  ||||    ||||      ||        ||||      ||||    ||||       || ||||");
+            Console.WriteLine("          ||||||     ||     ||      ||||    ||||       || ||||    ||    ||||      ||    ||  ||||      ||||    ||||       || ||||");
+            Console.WriteLine("            ||||||||||      ||||||||||      ||||         |||||      ||||||||      ||    ||    |||||||||       ||||         |||||");
+            Console.WriteLine("            ||||||||||      ||||||||||      ||||         |||||      ||||||        ||||||||    ||||||||        ||||         |||||");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("                                            Dungeon Adventure Game                                                              ");
+            Console.ResetColor();
+            Console.WriteLine("                                          Press any button to begin                                                             ");
+        }
+
+
+
     }
 }
