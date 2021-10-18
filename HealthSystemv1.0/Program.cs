7bf4ea22e7072dc801beaf755db0a3b73c1a891f
Abigail Evans <cdn_abigail@outlook.com>
2021-10-17 23:11:30 -0300

    Take Damage
    Adding take damage method,
    trying to make a spill down effect.

--- HealthSystemv1.0/Program.cs
+++ HealthSystemv1.0/Program.cs
@@ -8,27 +8,60 @@ namespace HealthSystemv1._0
 {
     class Program
     {
-        static int health = 60;
+        static int weapon = 1;
+        static int health = 100;
         static int shield = 100;
         static int lives = 3;
         static int healthPotion = 20;
         static int currentHealthStatus = 0;
 
+        static int monsterAttack = 110;
+       
+        
+
         static string[] healthStatus = new string[5];
+        static string[] weaponName = new string[5];
         static void Main(string[] args)
         {
-            healthStatus[0] = "Imminent Danger";
-            healthStatus[1] = "Badly Hurt";
-            healthStatus[2] = "Hurt";
-            healthStatus[3] = "Healthy";
-            healthStatus[4] = "Perfectly Healthy";
+
+            // Health Status Array Strings 
+            healthStatus[0] = " <Imminent Danger>";
+            healthStatus[1] = " <Badly Hurt>";
+            healthStatus[2] = " <Hurt>";
+            healthStatus[3] = " <Healthy>";
+            healthStatus[4] = " <Perfectly Healthy>";
+
+            // Weapon Name Array Strings 
+            weaponName[4] = "Long Sword";
+            weaponName[3] = "Battle Axe";
+            weaponName[2] = "  Daggers";
+            weaponName[1] = "   Spear";
+            weaponName[0] = "   club";
 
 
 
-            StartScreen();
-            Console.ReadKey(true);
+            TakeDamage(0);
             ShowHUD();
             Console.ReadKey(true);
+        }
+
+        static void TakeDamage(int damage)
+        {
+            shield = shield - monsterAttack;
+
+            if (shield <0)
+            {
+                health = (shield - monsterAttack) + health;
+                shield = 0;
+
+                Console.WriteLine("this is working");
+                
+            }
+        }
+
+        static void ChangeWeapon(int currentWeapon)
+        {
+            weapon = currentWeapon;
         }
 
         static void HealthStatusCheck()
@@ -84,9 +117,10 @@ namespace HealthSystemv1._0
             Console.WriteLine("");
             Console.WriteLine("                                                                                                                                 ");
             Console.WriteLine("+-----------------------------------+-------------------------------------------------------------------------------------------+");
-            Console.WriteLine("|   Lives: " + lives + "                        |   ---Heal---           <Restores " + healthPotion + " points of health>                                     |");
-            Console.WriteLine("|   Health: " + health + " Health Status: " + healthStatus[currentHealthStatus]);
-
+            Console.WriteLine("|   Lives:   " +  lives + "                      |      Heal              <Restores " + healthPotion + " points of health>                                     |");
+            Console.WriteLine("|   Health: " + health +  healthStatus[currentHealthStatus] + " |   Use Shield           <Protects you from incomming attacks>                              |");
+            Console.WriteLine("|   Shield:  " + shield + "                     |   " + weaponName[weapon] + "                                                                                 |");
+            Console.WriteLine("+-----------------------------------+-------------------------------------------------------------------------------------------+");
         }
 
         static void StartScreen()
